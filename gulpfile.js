4e0bf9c8ba92963b50fa1a27c35ae8219dd5fcac
FastBoyCoder <130761956+FastBoyCoder@users.noreply.github.com>
2023-10-05 01:41:25 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -105,3 +105,4 @@ gulp.task('watch', function() {
     gulp.watch(paths.src.js).on('change', browserSync.reload);
     gulp.watch(paths.src.html).on('change', browserSync.reload);
 });
+exports.build = series(scssTask, jsTask);
